416f04262b2854f503d7927ca605d72240aa53b9
dheita <dheitaraden@gmail.com>
2019-11-11 20:39:17 -0800

    tugas ATM

---
+++ algoritma/ATM.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	cout<<"pilih menu"<<endl;
+	cout<<"1.Tarik Dana"<<endl;
+	cout<<"2.Transfer Dana"<<endl;
+	cout<<"3.Cek Dana"<<endl;
+	
+	int Menu;
+	cin>>Menu;
+	if(Menu == 1 ) {cout<<"Tarik Dana";}
+	else if (Menu == 2) {cout<<"Transfer Dana";}
+	else if(Menu == 3){cout<<"Cek Dana";}
+	else if {cout<<"Menu Tidak Ada";}
+}
